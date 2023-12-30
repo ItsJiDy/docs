e373cca09aaac5043501a281fa3865dd91bdc057
ItsJiDy <96864066+ItsJiDy@users.noreply.github.com>
2023-12-30 10:23:31 +0800

    Add files via upload

--- Neko/index.js
+++ Neko/index.js
@@ -2,6 +2,15 @@
     "use strict";
     const f = a.findByProps("sendMessage", "receiveMessage"),
         i = a.findByProps("Messages")
+        
+    function httpGet(theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", theUrl, false );
+        xmlHttp.send( null );
+        return xmlHttp.responseText;
+    }
+
     let c = [];
     var g = {
         onLoad: function() {
@@ -15,8 +24,9 @@
                 inputType: 1,
                 type: 1,
                 execute: function(e, n) {
+                    let Neko = JSON.parse(httpGet('https://nekos.best/api/v2/neko')).results[0].url;
                     f.sendMessage(n.channel.id, {
-                        content: 'plugin test'
+                        content: Neko
                     })
                 }
             }))
